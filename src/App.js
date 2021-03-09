08115d40816dc338c82109f327eb3e7aaaca4520
Naveen Tyagi <naveen.tyagi@fullestop.com>
2021-03-09 10:43:27 +0530

    Props vs State

--- src/App.js
+++ src/App.js
@@ -3,13 +3,34 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons : [
+      { name  : 'Max',  age : 28},
+      { name  : 'Manu',  age : 29},
+      { name  : 'Stephanie',  age : 26}
+    ],
+    otherState: 'some other value'
+  };
+
+  switchHandler = ()  =>  {
+    this.setState({
+      persons:  [
+        { name  : 'Maximillian', age  : 28},
+        { name  : 'Manu',  age : 29},
+        { name  : 'Stephanie',  age : 27}
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <div>Some Content</div>
-        <Person name="Max" age="28"/>
-        <Person name="Manu" age="29">My Hobbies:  Racing</Person>
-        <Person name="Stephanie"  age="26"/>
+        <button onClick={this.switchHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies:  Racing</Person>
+        <Person name={this.state.persons[2].name}  age={this.state.persons[2].age}/>
       </div>
     );
 
