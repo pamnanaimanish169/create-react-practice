b06ab52e45fc22819e85c6a4203255a709a75262
Naveen Tyagi <naveen.tyagi@fullestop.com>
2021-03-09 10:06:07 +0530

    Save Work

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Header from './Header/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
         <div>Some Content</div>
+        <Person name="Max" age="28"/>
+        <Person name="Manu" age="29">My Hobbies:  Racing</Person>
+        <Person name="Stephanie"  age="26"/>
       </div>
     );
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const person = () => {
-    return <p>I'm a Person!</p>
+// Using Functinoal based Components
+const person = (props) => {
+    return (
+    <div>
+        <p>I'm {props.name} and I am {props.age} years old!</p>
+        <p>{props.children}</p>
+    </div>
+    ) 
 };
+
+// Using Class Based COmponents
+// class person extends React.Component    {
+//     render()    {
+//         return  <p>Hello I am {this.props.name} and I am {this.props.age} years old!</p>
+//     }
+// }
 
 export default person;
